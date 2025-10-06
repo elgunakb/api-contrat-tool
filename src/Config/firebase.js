13cec5a3b22f9b2207e9c758e19170fbabea058e
elgunakbarli <elgunekberli200@gmail.com>
2025-10-06 22:01:39 +0400

    fixed db

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD3OhiCGWuTPA4hdbXxCYx2CxvTewdNL1w",
-    authDomain: "api-contrat-tool.firebaseapp.com",
-    projectId: "api-contrat-tool",
-    storageBucket: "api-contrat-tool.appspot.com",
-    messagingSenderId: "247461170416",
-    appId: "1:247461170416:web:d64aae47e09c7d09a08635",
-   databaseURL: "https://api-contrat-tool-default-rtdb.europe-west1.firebasedatabase.app/",
+    apiKey: 'AIzaSyCbXxVUsQRw3lsySOBegwsiJ2KDeSz8quI',
+    authDomain: 'featuredb25.firebaseapp.com',
+    projectId: 'featuredb25',
+    storageBucket: 'featuredb25.firebasestorage.app',
+    messagingSenderId: '850336681205',
+    appId: '1:850336681205:web:5add99852d610ef09089bc',
+    databaseURL: 'https://featuredb25-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,7 +18,7 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 export { auth, provider };
 
-// 
+//
 
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
